5f5b7420ed7dc32f726edfe8002cff551ec3fefb
Fabian <fabian.cast587@gmail.com>
2026-04-27 16:51:56 -0300

    Imprimir punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,20 @@
 
 int main()
 {
-    printf("hola mundo");
+    printf("hola mundo \n");
+
+    int variable = 100;
+    int *puntero = &variable;
+
+    printf("1) Contenido del puntero: %d\n", *puntero);
+
+    printf("2) Direccion almacenada por el puntero: %p\n", &puntero);
+
+    printf("3) Direccion de memoria de la variable: %p\n", &variable);
+
+    printf("4) Direccion de memoria del puntero: %p\n", &puntero);
+
+    printf("5) Tamaño de memoria de la variable: %zu bytes\n", variable);
 
     return 0;
 }
